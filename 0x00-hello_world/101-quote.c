91d6dc85757cfcf09908f742a4ccdc08321cec62
zjoart <oluwadahunsiifeoluwa02@gmail.com>
2022-10-14 14:32:37 -0700

    Hello world tasks completed

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - to be called
+* Return: 1 for the int
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
